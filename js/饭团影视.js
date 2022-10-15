9045400ac2a4bf030ae56e5726bc491c1e86d3c3
Karlin97 <61130555+Karlin97@users.noreply.github.com>
2022-10-15 17:09:08 +0800

    Add files via upload

---
+++ js/饭团影视.js
@@ -0,0 +1,18 @@
+muban.首图2.一级.href = 'h4.title text-overflow;a&&href';
+muban.首图2.二级.tabs = '.stui-pannel__bd h3';
+muban.首图2.二级.lists = '.play-content:eq(#id) li';
+muban.首图2.二级.title = '.stui-player__detail .title&&Text';
+muban.首图2.二级.content = '.data-more&&Text';
+var rule = Object.assign(muban.首图2,{
+    title:'饭团影视',
+    host:'https://www.fantuanhd.com',
+    url:'/type/id-fyclass-fypage.html',
+    searchUrl:'/search/page/fypage/wd/**.html',
+    class_name:'电 影&电视剧&综艺&动漫&纪录片',
+    class_url:'20&21&22&23&24',
+searchable:1,
+quickSearch:1,
+搜索:'li.stui-vodlist__item;a&&title;a&&data-original;.pic-text&&Text;a&&href'
+});
+  
+  

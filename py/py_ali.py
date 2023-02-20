1868644121af9dd6d6c6bf1bdffb6acff1f3d407
Karlin97 <61130555+Karlin97@users.noreply.github.com>
2023-02-20 11:03:12 +0800

    Add files via upload

--- py/py_ali.py
+++ py/py_ali.py
@@ -394,7 +394,7 @@ class Spider(Spider):  # 元类 默认的元类 type
 		url = 'https://api.aliyundrive.com/token/refresh'
 		if len(self.authorization) == 0 or self.timeoutTick - self.localTime <= 600:
 			form = {
-				'refresh_token':'98ffb844afe041d38f54e266c4f91b52'
+				'refresh_token':'104dfb1901cc4e1eb88a4594ca29ecaa'
 			}
 			if len(self.extend) > 0:
 				form['refresh_token'] = self.extend
